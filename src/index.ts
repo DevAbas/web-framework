ad4aeaa1601541ef78323c84df8987653101e7a4
DevAbas <abas.memmedov.94@gmail.com>
2020-03-27 00:14:00 +0400

    save method added

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
 import { User } from './models/User';
 
-const user = new User({ id: 1 });
+const user = new User({ name: 'ALSO NEW NAME', age: 8989 });
 
-user.fetch();
-
-setTimeout(() => {
-  console.log(user);
-}, 4000);
+user.save();
