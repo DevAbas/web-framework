bcd67198f834ec2a80ad766bcca5d5d301464987
DevAbas <abas.memmedov.94@gmail.com>
2020-03-27 16:34:21 +0400

    Created Sync class

---
+++ src/models/Sync.ts
@@ -0,0 +1,21 @@
+import axios, { AxiosResponse } from 'axios';
+import { UserProps } from './User';
+
+export class Sync {
+  constructor(public rootUrl: string) {}
+
+  fetch(id: number): void {
+    axios.get(`${this.rootUrl}/${id}`).then((response: AxiosResponse) => {
+      return response.data;
+    });
+  }
+
+  save(data: UserProps): void {
+    const { id } = data;
+    if (id) {
+      axios.put(`${this.rootUrl}/${id}`, data);
+    } else {
+      axios.post(`${this.rootUrl}`, data);
+    }
+  }
+}
