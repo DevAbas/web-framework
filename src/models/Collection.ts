62ef6fbe16d6fee980f4cab0d3cac2f52be4af9e
DevAbas <abas.memmedov.94@gmail.com>
2020-03-29 16:32:05 +0400

    Reference type changed to Generic type

--- src/models/Collection.ts
+++ src/models/Collection.ts
@@ -1,12 +1,11 @@
 import axios, { AxiosResponse } from 'axios';
-import { User, UserProps } from './User';
 import { Eventing } from './Eventing';
 
-export class Collection<T> {
-  models: User[] = [];
+export class Collection<T, K> {
+  models: T[] = [];
   events: Eventing = new Eventing();
 
-  constructor(public rootUrl: string) {}
+  constructor(public rootUrl: string, public deserialize: (json: K) => T) {}
 
   get on() {
     return this.events.on;
@@ -18,10 +17,11 @@ export class Collection<T> {
 
   fetch(): void {
     axios.get(this.rootUrl).then((response: AxiosResponse) => {
-      response.data.map((value: UserProps) => {
-        const user = User.buildUser(value);
-        this.models.push(user);
+      response.data.map((value: K) => {
+        this.models.push(this.deserialize(value));
       });
     });
+
+    this.trigger('change');
   }
 }
