a6eb5b15762f31609a1dbdd52eefec39f143b070
DevAbas <abas.memmedov.94@gmail.com>
2020-03-31 21:38:02 +0400

    Added click event listener to button on rendered template

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -11,11 +11,24 @@ export class UserForm {
     console.log('Hello world')
   }
 
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+
+    for(let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(':')
+      
+      fragment.querySelectorAll(selector).forEach(element => {
+        element.addEventListener(eventName, eventsMap[eventKey])
+      })
+    }
+  }
+
   template(): string {
     return`
       <div>
         <h1>User Form</h1>
         <input />
+        <button>Click me</button>
       </div>
     `
   }
@@ -23,6 +36,7 @@ export class UserForm {
   render():void {
     const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
+    this.bindEvents(templateElement.content)
     this.parent.append(templateElement.content)
   }
 }
