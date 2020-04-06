e124633cbeb1721f3f4887577924f61195858109
DevAbas <abas.memmedov.94@gmail.com>
2020-04-06 23:49:43 +0400

    Generics refactor approach implemented on View

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,16 +1,9 @@
-import { User } from './../models/User';
+import { User, UserProps } from './../models/User';
+import { View } from './View';
 
-export class UserForm {
-  constructor(public parent: Element, public model: User){
-    this.bindModel()
-  }
 
-  bindModel():void {
-    this.model.on('change', () => {
-      this.render();
-    })
-  }
-
+export class UserForm extends View<User, UserProps> {
+  
   eventsMap(): { [key: string]: () => void } {
     return {
       'click:.set-age': this.onSetAgeClick,
@@ -33,17 +26,6 @@ export class UserForm {
     
   }
 
-  bindEvents(fragment: DocumentFragment): void {
-    const eventsMap = this.eventsMap();
-
-    for(let eventKey in eventsMap) {
-      const [eventName, selector] = eventKey.split(':')
-      
-      fragment.querySelectorAll(selector).forEach(element => {
-        element.addEventListener(eventName, eventsMap[eventKey])
-      })
-    }
-  }
 
   template(): string {
     return`
@@ -56,13 +38,5 @@ export class UserForm {
         <button class="set-age">Set User Age</button>
       </div>
     `
-  }
-
-  render():void {
-    this.parent.innerHTML = ''
-    const templateElement = document.createElement('template');
-    templateElement.innerHTML = this.template();
-    this.bindEvents(templateElement.content)
-    this.parent.append(templateElement.content)
   }
 }
